deb839d1251c1f19780feaee8a01b947a6e41e4c
tvpeters <mail@tvpeters.ru>
2021-06-10 10:46:38 +0300

    failed attempt to configure pagination in the slider

--- js/script.js
+++ js/script.js
@@ -1,7 +1,34 @@
-const swiper = new Swiper('.swiper-container', {
+'use strict';
+
+// swiper
+
+let swiper = new Swiper('.swiper-container', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
+    autoHeight: true,
+    centerInsufficientSlides: true,
+    centeredSlides: true,
+    // centeredSlidesBounds: true,
+    createElements: true,
+    // Responsive breakpoints
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 20
+        },
+        // when window width is >= 480px
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 30
+        },
+        // when window width is >= 1024px
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 40
+        }
+    },
     // If we need pagination
     pagination: {
         el: '.swiper-pagination',
@@ -15,4 +42,13 @@ const swiper = new Swiper('.swiper-container', {
     scrollbar: {
         el: '.swiper-scrollbar',
     },
+});
+
+// menu
+
+let mainNav = document.getElementById("js-menu");
+let navBarToggle = document.getElementById("js-navbar-toggle");
+
+navBarToggle.addEventListener("click", function() {
+    mainNav.classList.toggle("active");
 });
